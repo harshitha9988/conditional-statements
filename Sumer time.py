ae04cabae2f26ebac8b9dbabe26bfe7ae78cdd9d
harshitha9988 <harshitha.uppala@outlook.com>
2025-01-18 13:38:06 -0500

    sumer time program

---
+++ Sumer time.py
@@ -0,0 +1,10 @@
+x=int(input("Enter the temperature in farenhieght "))
+
+if (x>98):
+    print("It seems to be warm! Have fun outside!")
+
+if (x==98):
+    print("It's a nice day outside!")
+
+else:
+    print("It is cold, make sure to have proper clothing!")
